6399a9c64329e5b78332fe73eb5eeff191ed22b0
SoUuzaaa <felipebrasilsantos@outlook.com>
2021-01-21 23:38:39 -0300

    Initial JS

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+// Dark Mode 
+
 const html = document.querySelector('html')
 const checkbox = document.getElementById('checkbox')
 
 checkbox.addEventListener('change', () => {
     html.classList.toggle('dark')
 })
+
+// Script
+
+const Modal = {
+    open(){
+        // Abrir Modal
+        // Adicionar a class active ao modal
+        document.querySelector('.modal-overlay').classList.add('active')
+    },
+    close(){
+        // Fechar Modal
+        // Remover a class active do modal
+        document.querySelector('.modal-overlay').classList.remove('active')
+    }
+}
